8b5f501adfab4a546a4231539736ba9f9860765d
R4don1T19 <311372871+R4don1T19@users.noreply.github.com>
2026-08-08 14:17:48 +0300

    Modify and optimize ParallaxEffect.cs

---
+++ Assets/Scripts/Camera Stuff/ParallaxEffect.cs
@@ -0,0 +1,39 @@
+using System.Reflection.Metadata.Ecma335;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject Camera;
+    [SerializeField] private float Parallax;
+    private Vector2 StartPosition;
+    private bool _isActive = false;
+    private void Start()
+    {
+        StartPosition = gameObject.transform.position;
+    }
+    private void Update()
+    {
+        if (!_isActive)
+            return;
+        else
+        {
+            Vector2 CameraPosition = Camera.transform.position;
+            float ParallaxFactor = 1 - Parallax;
+
+            float PosX = StartPosition.x + (CameraPosition.x - StartPosition.x) * ParallaxFactor;
+            float PosY = StartPosition.y + (CameraPosition.y - StartPosition.y) * ParallaxFactor;
+
+            gameObject.transform.position = new Vector2(PosX, PosY);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MainCamera"))
+            _isActive = true;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("MainCamera"))
+            _isActive = false;
+    }
+}
