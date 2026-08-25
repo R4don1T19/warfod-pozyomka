d58d98861be9c6358d495d1149a15f411b9d9ae9
R4don1T19 <311372871+R4don1T19@users.noreply.github.com>
2026-08-25 15:09:38 +0300

    End new Mechanic for small-talk windows

--- Assets/Scripts/SDW/SmallDialogueWindowManager.cs
+++ Assets/Scripts/SDW/SmallDialogueWindowManager.cs
@@ -1,25 +1,66 @@
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
-public class SmallDialogueWindowManager : MonoBehaviour 
+using Unity.VisualScripting;
+public class SmallDialogueWindowManager : MonoBehaviour
 {
     [SerializeField] private DialogueUI dUI;
     [SerializeField] private SmallDialogueWindowPlayerTriggerZone pTZ;
+    [SerializeField] private PlayerMovement PM;
     [SerializeField] private GameObject DIalogueBox;
     [SerializeField] private TMP_Text TextLine;
     [SerializeField] private TMP_Text SpeakerName;
     [SerializeField] private Image SpeakerSprite;
+    private SmallDialogueWindowLine currentLine;
+    private bool DialogueIsActive = false;
+    private int LineCount = 0;
+    private int CurrentNode = 0;
     private void Update()
     {
         if (pTZ.StartInspect)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.E) && !DialogueIsActive)
                 StartInspect(pTZ.Graph);
+            else if (Input.GetKeyDown(KeyCode.E) && DialogueIsActive)
+                DialogueEnd();
         }
     }
 
     private void StartInspect(SmallDialogueWindowGraph graph)
     {
-        Debug.Log(graph.NodesList[0].lines[0].line);
+        PM.enabled = false;
+        // Все нужны для UI-элементов значения берутся напрямую именно с самого последнего списка(реплика, спрайт и имя говорящего).
+        currentLine = graph.NodesList[CurrentNode].lines[LineCount];
+
+        DIalogueBox.SetActive(true);
+        SpeakerName.text = currentLine.name;
+        SpeakerSprite.sprite = currentLine.icon;
+        TextLine.text = currentLine.line;
+        LineCount++;
+
+        if (LineCount >= graph.NodesList[CurrentNode].lines.Count)
+        {
+            LineCount = 0;
+            CurrentNode++;
+            if (CurrentNode < graph.NodesList.Count)
+                return;
+            else
+            {
+                CurrentNode = 0;
+                LineCount = 0;
+                DialogueIsActive = true;
+                return;
+            }
+        }
+    }
+    private void DialogueEnd()
+    {
+        SpeakerName.text = null;
+        SpeakerSprite.sprite = null;
+        TextLine.text = null;
+        DIalogueBox.SetActive(false);
+
+        DialogueIsActive = false;
+        PM.enabled = true;
     }
 }
